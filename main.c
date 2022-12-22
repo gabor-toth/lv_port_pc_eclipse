1fed4fbd7c862511b0d1eec35ccddfd37df2b3d9
Gabor Toth <gabor.toth@p92.hu>
2022-12-23 00:28:16 +0100

    play around

--- main.c
+++ main.c
@@ -24,11 +24,17 @@
 /**********************
  *      TYPEDEFS
  **********************/
+typedef enum {
+    DISP_SMALL,
+    DISP_MEDIUM,
+    DISP_LARGE,
+} disp_size_t;
 
 /**********************
  *  STATIC PROTOTYPES
  **********************/
 static void hal_init(void);
+static void meter_create();
 
 /**********************
  *  STATIC VARIABLES
@@ -53,6 +59,20 @@ static void hal_init(void);
 /**********************
  *      VARIABLES
  **********************/
+
+static disp_size_t disp_size;
+
+static lv_obj_t * tv;
+
+static lv_style_t style_title;
+static lv_style_t style_bullet;
+
+static lv_obj_t * meter1;
+static lv_obj_t * meter2;
+static lv_obj_t * meter3;
+
+static const lv_font_t * font_large;
+static const lv_font_t * font_normal;
 
 /**********************
  *  STATIC PROTOTYPES
@@ -91,7 +111,8 @@ int main(int argc, char **argv)
 //  lv_example_flex_3();
 //  lv_example_label_1();
 
-    lv_demo_widgets();
+//    lv_demo_widgets();
+  meter_create();
 
   while(1) {
       /* Periodically call the lv_task handler.
@@ -101,6 +122,251 @@ int main(int argc, char **argv)
   }
 
   return 0;
+}
+
+static lv_obj_t * create_meter_box(lv_obj_t * parent, const char * title, const char * text1, const char * text2,
+                                   const char * text3)
+{
+    lv_obj_t * cont = lv_obj_create(parent);
+    lv_obj_set_height(cont, LV_SIZE_CONTENT);
+    lv_obj_set_flex_grow(cont, 1);
+
+//    lv_obj_t * title_label = lv_label_create(cont);
+//    lv_label_set_text(title_label, title);
+//    lv_obj_add_style(title_label, &style_title, 0);
+
+    lv_obj_t * meter = lv_meter_create(cont);
+    lv_obj_remove_style(meter, NULL, LV_PART_MAIN);
+    lv_obj_remove_style(meter, NULL, LV_PART_INDICATOR);
+    lv_obj_set_width(meter, LV_PCT(100));
+
+//    lv_obj_t * bullet1 = lv_obj_create(cont);
+//    lv_obj_set_size(bullet1, 13, 13);
+//    lv_obj_remove_style(bullet1, NULL, LV_PART_SCROLLBAR);
+//    lv_obj_add_style(bullet1, &style_bullet, 0);
+//    lv_obj_set_style_bg_color(bullet1, lv_palette_main(LV_PALETTE_RED), 0);
+//    lv_obj_t * label1 = lv_label_create(cont);
+//    lv_label_set_text(label1, text1);
+//
+//    lv_obj_t * bullet2 = lv_obj_create(cont);
+//    lv_obj_set_size(bullet2, 13, 13);
+//    lv_obj_remove_style(bullet2, NULL, LV_PART_SCROLLBAR);
+//    lv_obj_add_style(bullet2, &style_bullet, 0);
+//    lv_obj_set_style_bg_color(bullet2, lv_palette_main(LV_PALETTE_BLUE), 0);
+//    lv_obj_t * label2 = lv_label_create(cont);
+//    lv_label_set_text(label2, text2);
+//
+//    lv_obj_t * bullet3 = lv_obj_create(cont);
+//    lv_obj_set_size(bullet3, 13, 13);
+//    lv_obj_remove_style(bullet3,  NULL, LV_PART_SCROLLBAR);
+//    lv_obj_add_style(bullet3, &style_bullet, 0);
+//    lv_obj_set_style_bg_color(bullet3, lv_palette_main(LV_PALETTE_GREEN), 0);
+//    lv_obj_t * label3 = lv_label_create(cont);
+//    lv_label_set_text(label3, text3);
+
+    if(disp_size == DISP_MEDIUM) {
+        static lv_coord_t grid_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_FR(1), LV_GRID_CONTENT, LV_GRID_FR(8), LV_GRID_TEMPLATE_LAST};
+        static lv_coord_t grid_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_FR(1), LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
+
+        lv_obj_set_grid_dsc_array(cont, grid_col_dsc, grid_row_dsc);
+//        lv_obj_set_grid_cell(title_label, LV_GRID_ALIGN_START, 0, 4, LV_GRID_ALIGN_START, 0, 1);
+        lv_obj_set_grid_cell(meter, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 1, 3);
+//        lv_obj_set_grid_cell(bullet1, LV_GRID_ALIGN_START, 2, 1, LV_GRID_ALIGN_CENTER, 2, 1);
+//        lv_obj_set_grid_cell(bullet2, LV_GRID_ALIGN_START, 2, 1, LV_GRID_ALIGN_CENTER, 3, 1);
+//        lv_obj_set_grid_cell(bullet3, LV_GRID_ALIGN_START, 2, 1, LV_GRID_ALIGN_CENTER, 4, 1);
+//        lv_obj_set_grid_cell(label1, LV_GRID_ALIGN_STRETCH, 3, 1, LV_GRID_ALIGN_CENTER, 2, 1);
+//        lv_obj_set_grid_cell(label2, LV_GRID_ALIGN_STRETCH, 3, 1, LV_GRID_ALIGN_CENTER, 3, 1);
+//        lv_obj_set_grid_cell(label3, LV_GRID_ALIGN_STRETCH, 3, 1, LV_GRID_ALIGN_CENTER, 4, 1);
+    }
+    else {
+        static lv_coord_t grid_col_dsc[] = {LV_GRID_CONTENT, LV_GRID_FR(1), LV_GRID_TEMPLATE_LAST};
+        static lv_coord_t grid_row_dsc[] = {LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_CONTENT, LV_GRID_TEMPLATE_LAST};
+        lv_obj_set_grid_dsc_array(cont, grid_col_dsc, grid_row_dsc);
+//        lv_obj_set_grid_cell(title_label, LV_GRID_ALIGN_START, 0, 2, LV_GRID_ALIGN_START, 0, 1);
+        lv_obj_set_grid_cell(meter, LV_GRID_ALIGN_START, 0, 2, LV_GRID_ALIGN_START, 1, 1);
+//        lv_obj_set_grid_cell(bullet1, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 2, 1);
+//        lv_obj_set_grid_cell(bullet2, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 3, 1);
+//        lv_obj_set_grid_cell(bullet3, LV_GRID_ALIGN_START, 0, 1, LV_GRID_ALIGN_START, 4, 1);
+//        lv_obj_set_grid_cell(label1, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_START, 2, 1);
+//        lv_obj_set_grid_cell(label2, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_START, 3, 1);
+//        lv_obj_set_grid_cell(label3, LV_GRID_ALIGN_STRETCH, 1, 1, LV_GRID_ALIGN_START, 4, 1);
+    }
+
+
+    return meter;
+
+}
+
+static void meter_create() {
+    lv_meter_scale_t * scale;
+    lv_meter_indicator_t * arc;
+    lv_meter_indicator_t * indic;
+
+    if(LV_HOR_RES <= 320) disp_size = DISP_SMALL;
+    else if(LV_HOR_RES < 720) disp_size = DISP_MEDIUM;
+    else disp_size = DISP_LARGE;
+
+    font_large = LV_FONT_DEFAULT;
+    font_normal = LV_FONT_DEFAULT;
+
+    lv_coord_t tab_h;
+    if(disp_size == DISP_LARGE) {
+        tab_h = 70;
+#if LV_FONT_MONTSERRAT_24
+        font_large     = &lv_font_montserrat_24;
+#else
+        LV_LOG_WARN("LV_FONT_MONTSERRAT_24 is not enabled for the widgets demo. Using LV_FONT_DEFAULT instead.");
+#endif
+#if LV_FONT_MONTSERRAT_16
+        font_normal    = &lv_font_montserrat_16;
+#else
+        LV_LOG_WARN("LV_FONT_MONTSERRAT_16 is not enabled for the widgets demo. Using LV_FONT_DEFAULT instead.");
+#endif
+    }
+    else if(disp_size == DISP_MEDIUM) {
+        tab_h = 45;
+#if LV_FONT_MONTSERRAT_20
+        font_large     = &lv_font_montserrat_20;
+#else
+        LV_LOG_WARN("LV_FONT_MONTSERRAT_20 is not enabled for the widgets demo. Using LV_FONT_DEFAULT instead.");
+#endif
+#if LV_FONT_MONTSERRAT_14
+        font_normal    = &lv_font_montserrat_14;
+#else
+        LV_LOG_WARN("LV_FONT_MONTSERRAT_14 is not enabled for the widgets demo. Using LV_FONT_DEFAULT instead.");
+#endif
+    }
+    else {   /* disp_size == DISP_SMALL */
+        tab_h = 45;
+#if LV_FONT_MONTSERRAT_18
+        font_large     = &lv_font_montserrat_18;
+#else
+        LV_LOG_WARN("LV_FONT_MONTSERRAT_18 is not enabled for the widgets demo. Using LV_FONT_DEFAULT instead.");
+#endif
+#if LV_FONT_MONTSERRAT_12
+        font_normal    = &lv_font_montserrat_12;
+#else
+        LV_LOG_WARN("LV_FONT_MONTSERRAT_12 is not enabled for the widgets demo. Using LV_FONT_DEFAULT instead.");
+#endif
+    }
+
+#if LV_USE_THEME_DEFAULT
+    lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK,
+                          font_normal);
+#endif
+
+    lv_style_init(&style_title);
+    lv_style_set_text_font(&style_title, font_large);
+
+    lv_style_init(&style_bullet);
+    lv_style_set_border_width(&style_bullet, 0);
+    lv_style_set_radius(&style_bullet, LV_RADIUS_CIRCLE);
+
+    lv_obj_set_style_text_font(lv_scr_act(), font_normal, 0);
+
+    lv_obj_t * parent = lv_scr_act();
+    lv_obj_set_flex_flow(parent, LV_FLEX_FLOW_ROW_WRAP);
+
+    meter3 = create_meter_box(parent, "Network Speed", "Low speed", "Normal Speed", "High Speed");
+    if(disp_size < DISP_LARGE) {
+    	lv_obj_add_flag(lv_obj_get_parent(meter3), LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
+    }
+
+    /*Add a special circle to the needle's pivot*/
+//    lv_obj_set_style_pad_hor(meter3, 10, 0);
+//    lv_obj_set_style_size(meter3, 10, LV_PART_INDICATOR);
+//    lv_obj_set_style_radius(meter3, LV_RADIUS_CIRCLE, LV_PART_INDICATOR);
+//    lv_obj_set_style_bg_opa(meter3, LV_OPA_COVER, LV_PART_INDICATOR);
+//    lv_obj_set_style_bg_color(meter3, lv_palette_darken(LV_PALETTE_GREY, 4), LV_PART_INDICATOR);
+//    lv_obj_set_style_outline_color(meter3, lv_color_white(), LV_PART_INDICATOR);
+//    lv_obj_set_style_outline_width(meter3, 3, LV_PART_INDICATOR);
+    lv_obj_set_style_text_color(meter3, lv_palette_darken(LV_PALETTE_GREY, 1), LV_PART_TICKS);
+
+    // https://docs.lvgl.io/8.0/widgets/extra/meter.html
+
+    scale = lv_meter_add_scale(meter3);
+    lv_meter_set_scale_range(meter3, scale, 0, 100, 180, 360 - 180);
+    lv_meter_set_scale_ticks(meter3, scale, 21, 3, 17, lv_color_white());
+    lv_meter_set_scale_major_ticks(meter3, scale, 2, 4, 22, lv_color_white(), 15);
+
+    arc = lv_meter_add_arc(meter3, scale, 10, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_meter_set_indicator_start_value(meter3, arc, 0);
+    lv_meter_set_indicator_end_value(meter3, arc, 20);
+
+    arc = lv_meter_add_arc(meter3, scale, 10, lv_palette_main(LV_PALETTE_BLUE), 0);
+    lv_meter_set_indicator_start_value(meter3, arc, 20);
+    lv_meter_set_indicator_end_value(meter3, arc, 40);
+
+    arc = lv_meter_add_arc(meter3, scale, 10, lv_palette_main(LV_PALETTE_GREEN), 0);
+    lv_meter_set_indicator_start_value(meter3, arc, 40);
+    lv_meter_set_indicator_end_value(meter3, arc, 100);
+
+    arc = lv_meter_add_arc(meter3, scale, 10, lv_palette_main(LV_PALETTE_RED), -15);
+    lv_meter_set_indicator_start_value(meter3, arc, 0);
+    lv_meter_set_indicator_end_value(meter3, arc, 20);
+
+    arc = lv_meter_add_arc(meter3, scale, 10, lv_palette_main(LV_PALETTE_BLUE), -15);
+    lv_meter_set_indicator_start_value(meter3, arc, 20);
+    lv_meter_set_indicator_end_value(meter3, arc, 40);
+
+    arc = lv_meter_add_arc(meter3, scale, 10, lv_palette_main(LV_PALETTE_GREEN), -15);
+    lv_meter_set_indicator_start_value(meter3, arc, 40);
+    lv_meter_set_indicator_end_value(meter3, arc, 100);
+
+    indic = lv_meter_add_scale_lines(meter3, scale,
+                                     lv_palette_darken(LV_PALETTE_RED, 3),
+                                     lv_palette_darken(LV_PALETTE_RED, 3),
+                                     true,
+                                     0);
+    lv_meter_set_indicator_start_value(meter3, indic, 0);
+    lv_meter_set_indicator_end_value(meter3, indic, 20);
+
+
+    indic = lv_meter_add_scale_lines(meter3, scale, lv_palette_darken(LV_PALETTE_BLUE, 3),
+                                     lv_palette_darken(LV_PALETTE_BLUE, 3), true, 0);
+    lv_meter_set_indicator_start_value(meter3, indic, 20);
+    lv_meter_set_indicator_end_value(meter3, indic, 40);
+
+
+    indic = lv_meter_add_scale_lines(meter3, scale, lv_palette_darken(LV_PALETTE_GREEN, 3),
+                                     lv_palette_darken(LV_PALETTE_GREEN, 3), true, 0);
+    lv_meter_set_indicator_start_value(meter3, indic, 40);
+    lv_meter_set_indicator_end_value(meter3, indic, 100);
+
+//    indic = lv_meter_add_needle_line(meter3, scale, 4, lv_palette_darken(LV_PALETTE_GREY, 4), -25);
+
+//    lv_obj_t * mbps_label = lv_label_create(meter3);
+//    lv_label_set_text(mbps_label, "-");
+//    lv_obj_add_style(mbps_label, &style_title, 0);
+
+//    lv_obj_t * mbps_unit_label = lv_label_create(meter3);
+//    lv_label_set_text(mbps_unit_label, "Mbps");
+
+//    lv_anim_init(&a);
+//    lv_anim_set_values(&a, 10, 60);
+//    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+//    lv_anim_set_exec_cb(&a, meter3_anim_cb);
+//    lv_anim_set_var(&a, indic);
+//    lv_anim_set_time(&a, 4100);
+//    lv_anim_set_playback_time(&a, 800);
+//    lv_anim_start(&a);
+
+    lv_obj_update_layout(parent);
+    if(disp_size == DISP_MEDIUM) {
+//        lv_obj_set_size(meter1, 200, 200);
+//        lv_obj_set_size(meter2, 200, 200);
+        lv_obj_set_size(meter3, 200, 200);
+    }
+    else {
+        lv_coord_t meter_w = lv_obj_get_width(meter3);
+//        lv_obj_set_height(meter1, meter_w);
+//        lv_obj_set_height(meter2, meter_w);
+        lv_obj_set_height(meter3, meter_w);
+//        lv_obj_set_size(meter3, 300, 240);
+    }
+
+//    lv_obj_align(mbps_label, LV_ALIGN_TOP_MID, 10, lv_pct(55));
+//    lv_obj_align_to(mbps_unit_label, mbps_label, LV_ALIGN_OUT_RIGHT_BOTTOM, 10, 0);
 }
 
 /**********************
